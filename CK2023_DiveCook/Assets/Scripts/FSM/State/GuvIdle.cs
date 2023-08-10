e6e65f485a62f094c6fe5668217b88805cde31ee
VictoryIK12 <85018133+VictoryIK12@users.noreply.github.com>
2023-08-10 23:23:14 +0900

    Creating Boss Monster

---
+++ CK2023_DiveCook/Assets/Scripts/FSM/State/GuvIdle.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FSM;
+
+[CreateAssetMenu(fileName = "GuvIdle", menuName = "FSM/GuvIdle", order = 1)]
+public class GuvIdle : StateBase<Guv>
+{
+    [SerializeField] StateBase<Guv> MoveState;
+    [SerializeField] StateBase<Guv> DieState;
+    [SerializeField] StateBase<Guv> RushState;
+    [SerializeField] StateBase<Guv> FaintState; // 기절
+
+    public override void Enter(Guv guv)
+    {
+        Debug.Log("Idle State");
+    }
+
+    public override void Execute(Guv guv)
+    {
+        if(guv.CurrentHp <= 0)
+        {
+            guv.ChangeState(DieState);
+            return;
+        }
+        else if(guv)
+        {
+
+        }
+    }
+
+    public override void Exit(Guv guv)
+    {
+        
+    }
+}
