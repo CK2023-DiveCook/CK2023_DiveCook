bcb0428575dea337fc7420aca58afd36ef7ef29a
lowhook03 <lowhook03@gmail.com>
2023-08-12 14:32:08 +0900

    BG Move

--- CK2023_DiveCook/Assets/Scripts/Objects/BackGroundMove.cs
+++ CK2023_DiveCook/Assets/Scripts/Objects/BackGroundMove.cs
@@ -7,18 +7,18 @@ public class BackGroundMove : MonoBehaviour
     [SerializeField] private float xOffset;
     [SerializeField] private float yOffset;
     [SerializeField] private GameObject target;
-    private Vector3 _origin;
+    private Vector3 origin;
 
     // Start is called before the first frame update
     private void Start()
     {
-        _origin = transform.position;
+        origin = transform.position;
     }
 
     // Update is called once per frame
     private void Update()
     {
         var position = target.transform.position;
-        transform.position = new Vector3(_origin.x + (xOffset * position.x), _origin.y + (yOffset * position.y), _origin.z);
+        transform.position = new Vector3(origin.x + (xOffset * position.x), origin.y + (yOffset * position.y), origin.z);
     }
 }
