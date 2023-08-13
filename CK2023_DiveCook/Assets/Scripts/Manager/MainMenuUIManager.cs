2b42a533891f418f4428ca70f1382ed8a7720fca
Choi Seung Ik <krppbud12@naver.com>
2023-08-13 23:45:13 +0900

    Main Modify

--- CK2023_DiveCook/Assets/Scripts/Manager/MainMenuUIManager.cs
+++ CK2023_DiveCook/Assets/Scripts/Manager/MainMenuUIManager.cs
@@ -6,18 +6,53 @@ namespace Manager
 	public class MainMenuUIManager : MonoBehaviour
 	{
 		[SerializeField] private GameObject settingUI;
+        [SerializeField] private GameObject HowToUI;
+		[SerializeField] private GameObject LogOutUI;
 
-		private void Start()
+		[SerializeField] private GameObject YesButton;
+        [SerializeField] private GameObject NoButton;
+		[SerializeField] private GameObject howToExitButton;
+
+        private void Start()
 		{
 			settingUI.SetActive(false);
-		}
+            HowToUI.SetActive(false);
+			LogOutUI.SetActive(false);
+            YesButton.SetActive(false);
+			NoButton.SetActive(false);
+			howToExitButton.SetActive(false);
+        }
 
 		public void StartGame()
 		{
 			SceneManager.LoadScene("Game");
 		}
+		public void OpenHowto()
+		{
+			HowToUI.SetActive(true);
+            howToExitButton.SetActive(true);
+        }
+		public void CloseHowto()
+		{
+            HowToUI.SetActive(false);
+			howToExitButton.SetActive(false);
 
-		public void OpenSetting()
+        }
+		public void OpenLogOut()
+		{
+			LogOutUI.SetActive(true);
+            YesButton.SetActive(true);
+            NoButton.SetActive(true);
+        }
+		public void CloseLogOut()
+		{
+            LogOutUI.SetActive(false);
+            YesButton.SetActive(false);
+            NoButton.SetActive(false);
+
+        }
+
+        public void OpenSetting()
 		{
 			settingUI.SetActive(true);
 			DataManager.Instance.LoadAudioData();
@@ -29,5 +64,9 @@ namespace Manager
 			settingUI.SetActive(false);
 			DataManager.Instance.SaveAudioData();
 		}
+		public void ExitGame()
+		{
+            Application.Quit();
+        }
 	}
 }
